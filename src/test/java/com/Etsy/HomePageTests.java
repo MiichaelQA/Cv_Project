cffe6140c35027931b0c1ef79d60f49707e423e8
Moxail <qamichaelkorolev@gmail.com>
2020-07-15 21:10:02 +0300

    Last version with TestRail

--- src/test/java/com/Etsy/HomePageTests.java
+++ src/test/java/com/Etsy/HomePageTests.java
@@ -2,10 +2,13 @@ package com.Etsy;
 
 import com.Etsy.pages.ContextBarHelper;
 import com.Etsy.pages.HomePageHelper;
+import gurock.testrail.APIException;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class HomePageTests extends TestBase {
 
@@ -13,46 +16,61 @@ public class HomePageTests extends TestBase {
     ContextBarHelper contextBar;
 
     @BeforeMethod
-    public void initTests(){
-        homePage = PageFactory.initElements(driver,HomePageHelper.class);
-        contextBar = PageFactory.initElements(driver,ContextBarHelper.class);
+    public void initTests() {
+        homePage = PageFactory.initElements(driver, HomePageHelper.class);
+        contextBar = PageFactory.initElements(driver, ContextBarHelper.class);
     }
 
     @Test
-    public void VerifiHomePage(){
-        Assert.assertEquals(homePage.TitleVerification(),"What is Etsy?");
+    public void VerifiHomePage() {
+            Assert.assertEquals(homePage.TitleVerification(), "What is Etsy?");
     }
 
     @Test
-    public void SubscribeFieldUnicPositive(){
+    public void SubscribeFieldUnicPositive() throws APIException, IOException {
         homePage.waitUntilFieldSubsIsClicable();
         homePage.SubscribeField.sendKeys("miklomac@gmail.com");
         homePage.SubscribeButton.click();
         homePage.waitUntilElementIsVisible(homePage.UnicSubMail, 10);
+
+        if(homePage.verifyIfSubsMailUnic()){
+            TestBase.AddResultForTestCaseInTestRail("5", TEST_CASE_PASSED_STATUS);
+        } else {
+            TestBase.AddResultForTestCaseInTestRail("5", TEST_CASE_FAILED_STATUS);
+        }
         Assert.assertTrue(homePage.verifyIfSubsMailUnic());
     }
 
-    @Test
-    public void SubscribeFieldUsedPositive(){
+    @Test()
+    public void SubscribeFieldUsedPositive() throws APIException, IOException {
         homePage.waitUntilFieldSubsIsClicable();
         homePage.SubscribeField.sendKeys("logo@gmail.com");
         homePage.SubscribeButton.click();
         homePage.waitUntilElementIsVisible(homePage.UsedSubMail, 10);
+
+        if (homePage.verifyIfSubsMailUsed()){
+            TestBase.AddResultForTestCaseInTestRail("4", TEST_CASE_PASSED_STATUS);
+        } else {
+            TestBase.AddResultForTestCaseInTestRail("4", TEST_CASE_FAILED_STATUS);
+        }
         Assert.assertTrue(homePage.verifyIfSubsMailUsed());
+
     }
 
     @Test
-    public void TestContextMenu(){
+    public void TestContextMenu() throws APIException, IOException {
         builder.moveToElement(contextBar.CtgWeddingParty).perform();
         contextBar.waitUntilElementIsVisible(contextBar.SubCtgWeddingDecor,10);
         builder.moveToElement(contextBar.SubCtgWeddingDecor).perform();
         contextBar.waitUntilElementIsVisible(contextBar.SubCtgButtonCakeToppers,10);
         contextBar.SubCtgButtonCakeToppers.click();
+
+        if(contextBar.verifyContextFinish()) {
+            TestBase.AddResultForTestCaseInTestRail("6", TEST_CASE_PASSED_STATUS);
+        }else{
+            TestBase.AddResultForTestCaseInTestRail("6", TEST_CASE_FAILED_STATUS);
+        }
         Assert.assertTrue(contextBar.verifyContextFinish());
     }
-
-
-
-
 
 }
--- src/test/java/com/Etsy/TestBase.java
+++ src/test/java/com/Etsy/TestBase.java
@@ -4,9 +4,18 @@ import com.Etsy.pages.HomePageHelper;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import gurock.testrail.APIClient;
+import gurock.testrail.APIException;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+import org.json.simple.JSONObject;
 
 
 public class TestBase {
@@ -14,9 +23,28 @@ public class TestBase {
     public ChromeDriver driver = new ChromeDriver();
     protected static String baseUrl = "https://www.etsy.com/";
     public Actions builder = new Actions(driver);
-    public static final String LOGIN = "";
-    public static final String PASSWORD = "";
     HomePageHelper homePage;
+
+
+    public static String TestRailRunId    = "2" ;
+    public static String TestRailUsername = "qaautomationcvproject@gmail.com";
+    public static String TestRailPassword = "ue8h5hnORDN.wcGj9Rhh";
+    public static final int TEST_CASE_PASSED_STATUS = 1;
+    public static final int TEST_CASE_FAILED_STATUS = 5;
+    public static APIClient client = new APIClient("https://qaautomationcvprojectgmail.testrail.io/");
+
+
+    public static void AddResultForTestCaseInTestRail(String TestCaseId, int status)
+            throws IOException, APIException {
+        client.setUser(TestRailUsername);
+        client.setPassword(TestRailPassword);
+
+        Map data = new HashMap();
+        data.put("status_id", status);
+        data.put("comment", "This test worked fine!");
+        JSONObject r = (JSONObject) client.sendPost("add_result_for_case/" +TestRailRunId+ "/"+TestCaseId+"", data);
+    }
+
 
     @BeforeSuite
     public void initWebDriver(){
